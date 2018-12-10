231704eebce76f7ce1a6eb24630df06d41464a1d
anayatzirojas <44092257+anayatzirojas@users.noreply.github.com>
2018-12-10 12:22:18 -0500

    first commit

---
+++ lesson6/problem2/problem2.py
@@ -0,0 +1,6 @@
+name = input ('What is your name?')
+print ('yaya' + name +','' ' 'my name is carybot' ':)')
+age = input ('How old are you?')
+print ('Wow' + ',' + age + ' ''years old! I was born today but I wish I was already dead. HA HA human jokes about depressions are funny.')
+mood = input ('How was your day ?')
+print ('Great! that sound good')
